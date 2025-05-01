4a07a5f70fdc87ec87bbea0429dd90ab92e7423b
Cristianrb04 <cristian.ruiz5645@alumnos.udg.mx>
2025-05-01 17:56:14 -0600

    web sockets 1.2

--- models/image.js
+++ models/image.js
@@ -1,3 +1,4 @@
+/*
 const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
@@ -5,6 +6,22 @@ const imageSchema = new mongoose.Schema({
     url: { type: String, required: true },
     path: { type: String, required: true }, // Ruta del archivo o ubicación física
     key: { type: String, required: true },  // Clave única asociada a la imagen
+});
+
+const Image = mongoose.model('Image', imageSchema);
+
+module.exports = Image;
+*/
+
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+    path: { type: String, required: true },
+    key: { type: String, required: true },
+    clase: { type: String },         // ← NUEVO campo: persona / no persona
+    confidence: { type: Number }     // ← NUEVO campo: porcentaje (ej: 97.23)
 });
 
 const Image = mongoose.model('Image', imageSchema);
